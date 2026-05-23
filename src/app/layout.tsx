9c182d0ebab938363e7718a4a545b7fc33192c9a
poran-dip <porandip0@gmail.com>
2026-05-23 12:23:05 +0530

    ci: add dockerfile and dockerignore

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
-        <Analytics />
+        {process.env.VERCEL && <Analytics />}
       </body>
     </html>
   );
